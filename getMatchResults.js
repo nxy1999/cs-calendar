be30b9109f4f90317ae12073104808bef39b0424
nxy <2924157356@qq.com>
2024-03-21 22:32:19 +0800

    获取major赛事id

--- getMatchResults.js
+++ getMatchResults.js
@@ -1,9 +1,22 @@
 const { HLTV } = require('hltv')
 
 // 哥本哈根major的id
-const eventId = [7258,7148]
+// const eventId = [7258,7148]
+//
+// HLTV.getMatches({eventIds:eventId}).then((matches) => {
+//   // console.log(matches);
+//   console.log(JSON.stringify(matches));
+// })
 
-HLTV.getMatches({eventIds:eventId}).then((matches) => {
-  // console.log(matches);
-  console.log(JSON.stringify(matches));
-})
+// 构造一个包含major和INTLLAN类型的数组
+const eventTypes = ['MAJOR','INTLLAN'];
+
+// 获取事件ID
+HLTV.getEvents({ eventType: eventTypes }).then(res => {
+    const eventIds = res.map(event => event.id);
+
+    // 使用获取到的事件ID获取比赛信息
+    HLTV.getMatches({eventIds: eventIds}).then(matches => {
+        console.log(JSON.stringify(matches));
+    });
+});
