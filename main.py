05225b01a3b355e03ad84578851ef51f4f36fb6c
nxy <2924157356@qq.com>
2024-03-22 22:30:26 +0800

    代码重构，封装成函数

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from datetime import datetime
 
 # 星级推荐转换为星星符号
 def stars_to_symbols(stars):
-    star_symbol = '★'
-    return star_symbol * stars
+    return '★' * stars
 
 
 # 简单提取ICS内容中第一个SUMMARY的值
@@ -23,30 +22,6 @@ def extract_first_summary(ics_content):
             return line[len("SUMMARY:"):].strip()
     return None
 
-
-# def update_ics_file_if_needed(file_path, matches):
-#     """
-#     如果需要，根据新的比赛数据更新ICS文件。
-#     """
-#     # 生成新的ICS内容
-#     new_ics_content = generate_ics_content(matches)
-#     new_first_summary = extract_first_summary(new_ics_content)
-#
-#     # 尝试读取现有的ICS文件
-#     try:
-#         with open(file_path, 'r', encoding='utf-8') as file:
-#             old_ics_content = file.read()
-#         old_first_summary = extract_first_summary(old_ics_content)
-#     except FileNotFoundError:
-#         old_first_summary = None
-#
-#     # 如果第一个SUMMARY发生变化，或文件不存在，则更新ICS文件
-#     if new_first_summary != old_first_summary:
-#         with open(file_path, 'w', encoding='utf-8') as file:
-#             file.write(new_ics_content)
-#         print("ICS file has been updated.")
-#     else:
-#         print("First SUMMARY unchanged. No update needed.")
 
 def fetch_matches_data():
     """调用Node.js脚本获取比赛数据"""
@@ -77,37 +52,59 @@ def main():
     raise SystemExit
 
 
+def process_matches_data(matches_data):
+    """
+    # 不需要转换
+    # 批量转换'live'字段
+    for match in matches_data:
+        match['live'] = match.get('live')
+        # if 'live' in item and item['live'] == 'true':
+        #     item['live'] = True
+        # if item['live'] == 'false':
+        #     item['live'] = False
+    """
+
+    try:
+        with open('matches_calendar.ics', 'r', encoding='utf8') as file:
+            old_ics_content = file.read()
+    except FileNotFoundError:
+        old_ics_content = None
+
+    old_first_summary = extract_first_summary(old_ics_content)
+
+    cal = Calendar()
+
+    for match in matches_data:
+        if match['live']:  # 忽略正在进行的比赛
+            continue
+
+        try:
+            timestamp = match.get('date', 0) / 1000
+            begin_time = datetime.fromtimestamp(timestamp, pytz.timezone('Asia/Shanghai'))
+            team1_name = match['team1']['name']
+            team2_name = match['team2']['name']
+            event_name = f"{team1_name} vs {team2_name}"
+
+            if old_first_summary and old_first_summary == event_name:
+                print("First SUMMARY unchanged. No update needed.")
+                return
+
+            event = Event()
+            event.begin = begin_time
+            event.name = event_name
+            stars = match.get('stars', 0)
+            event.description = f"HLTV: {'★' * stars}\nHLTV: {match['stars']}星推荐\n赛制: {match['format']}\n赛事：{match['event']['name']}"
+            cal.events.add(event)
+        except KeyError as e:
+            print(f"Error processing match: {e}")
+
+    with open('matches_calendar.ics', 'w', encoding='utf8') as f:
+        f.write(cal.serialize())
+    print("日历文件创建成功！")
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-
-    matches_data = main()
-    print(matches_data)
-
-    # cycle = 0
-    # while True:
-    #     # 调用 Node.js 脚本获取比赛数据
-    #     node_script = "node getMatches.js"
-    #     result = subprocess.run(node_script, shell=True, capture_output=True, text=True)
-    #
-    #     if result.returncode == 0:
-    #         matches_json = result.stdout
-    #         # matches_data = {}
-    #         try:
-    #             matches_data = json.loads(matches_json)
-    #             break
-    #         except json.JSONDecodeError as e:
-    #             print(f"Error decoding JSON: {e}")
-    #     else:
-    #         print("Error running the Node.js script. Retrying in 5 seconds...")
-    #
-    #     # 休眠一段时间后重试
-    #     time.sleep(10)
-    #     cycle += 1
-    #     if cycle == 10:
-    #         print("获取比赛数据失败")
-    #         raise SystemExit
-
-    # print("成功获取比赛数据：", matches_data)
 
     # 测试数据
     # matches_data = [
@@ -121,13 +118,29 @@ if __name__ == '__main__':
     #      "event": {"id": 7726, "name": "Tipsport Winter Cup 2024 Finals"}, "live": False},
     #     {"id": 2370558, "date": 1710684000000, "stars": 0, "format": "bo3", "event": {"id": 7726, "name": "Tipsport Winter Cup 2024 Finals"}, "live": False}
     # ]
+    matches_data = main()
 
+    if matches_data:
+        process_matches_data(matches_data)
+
+    # print("成功获取比赛数据：", matches_data)
+
+'''
     # 批量修改
     for item in matches_data:
+        # item['live'] = item.get('live') == 'true'
         if 'live' in item and item['live'] == 'true':
             item['live'] = True
         if item['live'] == 'false':
             item['live'] = False
+
+    # 尝试读取现有的ICS文件
+    try:
+        with open('matches_calendar.ics', 'r', encoding='utf8') as file:
+            old_ics_content = file.read()
+        old_first_summary = extract_first_summary(old_ics_content)
+    except FileNotFoundError:
+        old_first_summary = None
 
     # 创建日历事件
     cal = Calendar()
@@ -156,14 +169,6 @@ if __name__ == '__main__':
             event_name = f"{team1_name} vs {team2_name}"
             event.name = event_name
 
-            # 尝试读取现有的ICS文件
-            try:
-                with open('matches_calendar.ics', 'r', encoding='utf8') as file:
-                    old_ics_content = file.read()
-                old_first_summary = extract_first_summary(old_ics_content)
-            except FileNotFoundError:
-                old_first_summary = None
-
             # 如果第一个SUMMARY发生变化，或文件不存在，则更新ICS文件
             if old_first_summary == event.name:
                 print("First SUMMARY unchanged. No update needed.")
@@ -191,3 +196,4 @@ if __name__ == '__main__':
         f.writelines(cal)
 
     print("日历文件创建成功！")
+'''
