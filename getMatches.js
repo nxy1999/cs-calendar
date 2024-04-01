7d27e271ca207139d49f82aa81aa5bf9d6ff6fc1
nxy <2924157356@qq.com>
2024-04-01 15:18:18 +0800

    添加返回值

--- getMatches.js
+++ getMatches.js
@@ -4,7 +4,7 @@ const { getEventIdsByType } = require('./getEventIdsByType.js');
 const { EventType } = require('hltv/lib/shared/EventType');
 /**
  * 获取指定类型的Major事件ID，然后获取这些事件的比赛信息
- * @returns {Promise<void>} 不返回任何内容
+ * @returns {Promise<import("./endpoints/getMatches").MatchPreview[]>} 不返回任何内容
  */
 async function getMatches() {
   try {
@@ -13,15 +13,15 @@ async function getMatches() {
     // 获取所有事件的比赛信息
     const matches = await HLTV.getMatches({ eventIds });
     console.log(JSON.stringify(matches));
+    return matches;
   } catch (error) {
     console.error("获取事件ID或比赛信息时发生错误:", error);
   }
 }
 
-// 因为是执行整个脚本的输出，所以必须有
 // 执行getMatches函数，并处理可能发生的未捕获错误
-getMatches().catch(error => {
-  console.error("getMatches函数中发生未处理的错误:", error);
-});
+// getMatches().catch(error => {
+//   console.error("getMatches函数中发生未处理的错误:", error);
+// });
 
 module.exports = { getMatches };
