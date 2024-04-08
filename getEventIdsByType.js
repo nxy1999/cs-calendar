5f96bb3bb0535bcc7b0ea933408880f181c0e248
nxy <2924157356@qq.com>
2024-04-08 23:19:05 +0800

    调整格式

--- getEventIdsByType.js
+++ getEventIdsByType.js
@@ -1,4 +1,4 @@
-const {HLTV} = require("hltv")
+const { HLTV } = require("hltv")
 
 /**
  * 将获取特定类型事件ID的逻辑封装为一个函数
@@ -6,27 +6,26 @@ const {HLTV} = require("hltv")
  * @param eventType
  */
 async function getEventIdsByType(eventType) {
-    if (!eventType) {
-        console.error(`Invalid event type string: ${eventType}`)
-        return Promise.reject(new Error(`Invalid event type string: ${eventType}`))
-    }
-    try {
-        const eventsResponse = await HLTV.getEvents({eventType})
-        // 处理只属于特定类型的事件
-        return eventsResponse.map((event) => event.id)
-    } catch (error) {
-        // 处理可能发生的错误
-        console.error("Error fetching event IDs by type:", error)
-        throw new Error("Failed to fetch event IDs")
-    }
+  if (!eventType) {
+    console.error(`Invalid event type string: ${eventType}`)
+    return Promise.reject(new Error(`Invalid event type string: ${eventType}`))
+  }
+  try {
+    const eventsResponse = await HLTV.getEvents({ eventType })
+    // 处理只属于特定类型的事件
+    return eventsResponse.map((event) => event.id)
+  } catch (error) {
+    // 处理可能发生的错误
+    console.error("Error fetching event IDs by type:", error)
+    throw new Error("Failed to fetch event IDs")
+  }
 }
 
 // 以下为测试代码，展示如何使用getEventIdsByType函数获取特定类型的事件ID
-// 必须注释掉，以免影响功能代码运行
 // 导入 EventType 枚举
 // const { EventType } = require("hltv/lib/shared/EventType")
 // getEventIdsByType(EventType.InternationalLAN)
-//     .then(eventIds => console.log('Event IDs:', eventIds))
-//     .catch(error => console.error('Error:', error));
+//   .then((eventIds) => console.log("Event IDs:", eventIds))
+//   .catch((error) => console.error("Error:", error))
 
-module.exports = {getEventIdsByType}
+module.exports = { getEventIdsByType }
