7b809880d5488ea9c1dfff9b292e52864ef21b47
nxy <2924157356@qq.com>
2024-05-15 11:13:33 +0800

    获取结果的文件

---
+++ getMatchesStats.js
@@ -0,0 +1,35 @@
+const { HLTV } = require("hltv")
+// 暂时没用
+/**
+ * 获取指定类型的Major事件ID，然后获取这些事件的比赛信息
+ * @returns {Promise<import("./endpoints/getMatches").MatchPreview[]>} 不返回任何内容
+ */
+async function getMatchesStats() {
+  // https://www.hltv.org/stats/matches?startDate=2024-04-15&endDate=2024-05-15&matchType=BigEvents
+  // 上个月的结果和BigEvents
+  try {
+    // // 获取事件ID
+    // const eventIds = await getEventIdsByType(EventType.InternationalLAN);
+    // 获取所有事件的比赛信息
+    const matches = await HLTV.getMatchesStats({
+      startDate: "2024-04-15",
+      endDate: "2024-05-15",
+      matchType: "BigEvents",
+    })
+    console.log(JSON.stringify(matches))
+    return matches
+  } catch (error) {
+    console.error("获取事件ID或比赛结果时发生错误:", error)
+  }
+}
+// 测试代码
+// const { getEventIdsByType } = require("./getEventIdsByType.js")
+const { MatchType } = require("hltv/lib/shared/MatchType")
+
+getMatchesStats(MatchType.BigEvents)
+  .then((matchesStats) => console.log(matchesStats))
+  .catch((error) =>
+    console.error("getMatchesStat函数中发生未处理的错误:", error),
+  )
+
+module.exports = { getMatchesStats }
---
+++ getResults.js
@@ -0,0 +1,28 @@
+const { HLTV } = require("hltv")
+
+/**
+ * 获取指定类型的Major事件ID，然后获取这些事件的比赛信息
+ * @returns {Promise<import("./endpoints/getMatches").MatchPreview[]>} 不返回任何内容
+ */
+async function getResults(eventIds) {
+  try {
+    // // 获取事件ID
+    // const eventIds = await getEventIdsByType(EventType.InternationalLAN);
+    // 获取所有事件的比赛信息
+    const results = await HLTV.getResults({ eventIds })
+    console.log(JSON.stringify(results))
+    return results
+  } catch (error) {
+    console.error("获取事件ID或比赛结果时发生错误:", error)
+  }
+}
+// 测试代码
+const { getEventIdsByType } = require("./getEventIdsByType.js")
+const { EventType } = require("hltv/lib/shared/EventType")
+getEventIdsByType(EventType.InternationalLAN).then((eventIds) => {
+  getResults(eventIds)
+    .then((matches) => console.log(matches))
+    .catch((error) => console.error("getResults函数中发生未处理的错误:", error))
+})
+
+module.exports = { getResults }
