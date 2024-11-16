0a17ff82d1ef318f3ac66bda93a440def67ae2cd
nxy <2924157356@qq.com>
2024-11-17 00:39:40 +0800

    feat: 添加 getMatchesByEvents函数
    - 新增 getMatchesByEvents.js 文件
    - 实现了获取指定事件的比赛信息的功能
    - 包含了测试代码，可以独立运行

---
+++ getMatchesByEvents.js
@@ -0,0 +1,46 @@
+const { HLTV } = require("hltv")
+// const { getDataUntilMaxTimes } = require("./utils")
+// const { getEventIdsByType } = require("./getEventIdsByType")
+// const { EventType } = require("hltv/lib/shared/EventType")
+/**
+ * 获取指定类型的Major事件ID，然后获取这些事件的比赛信息
+ * @returns
+ */
+async function getMatchesByEvents(eventIds) {
+  try {
+    // // 获取事件ID
+    // const eventIds = await getEventIdsByType(EventType.InternationalLAN);
+    // 获取所有事件的比赛信息
+    // const matches = await getDataUntilMaxTimes(
+    //   async () => await HLTV.getMatches({ eventIds }),
+    // )
+    // console.log("eventIds", eventIds)
+    const matches = await HLTV.getMatches({ eventIds })
+    console.log("matches", matches)
+    // console.log(JSON.stringify(matches))
+    console.log("matches[0]", matches[0])
+    return eventIds.includes(matches[0]?.event?.id) ? matches : []
+  } catch (error) {
+    console.error("获取事件ID或比赛信息时发生错误:", error)
+  }
+}
+
+// 测试代码
+if (require.main === module) {
+  const { getEventIdsByType } = require("./getEventIdsByType.js")
+  const { EventType } = require("hltv/lib/shared/EventType")
+
+  getEventIdsByType(EventType.InternationalLAN).then((eventIds) => {
+    getMatchesByEvents(eventIds)
+      .then((matches) => console.log("matches", matches))
+      .catch((error) =>
+        console.error("getMatches函数中发生未处理的错误:", error),
+      )
+  })
+}
+
+// getMatchesByEvents()
+//   .then((matches) => console.log(matches))
+//   .catch((error) => console.error("getMatches函数中发生未处理的错误:", error))
+
+module.exports = { getMatchesByEvents }
