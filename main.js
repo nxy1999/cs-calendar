39e0f1ec6bddcd653d491105ced7aa0b1aa8712b
nxy1999 <37256108+nxy1999@users.noreply.github.com>
2024-11-18 09:04:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ async function mainExecution(eventType) {
   //   throw new Error("未找到事件ID")
   // }
   // 如果可以并行处理，使用Promise.all；否则保持串行
-  const [matchesData, resultsData] = await Promise.all([
+  const [matchesData, resultsData = []] = await Promise.all([
     fetchAndProcessData(eventIds, "getMatchesByEvents"),
-    fetchAndProcessData(eventIds, "getResults"),
+    // fetchAndProcessData(eventIds, "getResults"),
   ])
   console.log(`[${new Date().getTime()}] 获取比赛数据成功`)
   // const resultsData = await main(eventType, getResults)
