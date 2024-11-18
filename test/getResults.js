47c39857ac995c7693225c7501751939cf80eb15
nxy <2924157356@qq.com>
2024-11-18 23:57:40 +0800

    refactor(test): 优化 getResults 函数代码结构
    -调整了 getResults 函数的代码缩进，提高了代码可读性
    - 修改了测试代码中的错误处理方式，使用箭头函数替代普通函数

--- test/getResults.js
+++ test/getResults.js
@@ -5,20 +5,22 @@ const { HLTV } = require("hltv")
  * @returns {Promise<import("./endpoints/getMatches").MatchPreview[]>} 不返回任何内容
  */
 async function getResults(eventIds) {
-  try {
-    // console.log(JSON.stringify(results))
-    return await HLTV.getResults({ eventIds })
-  } catch (error) {
-    console.error("获取事件ID或比赛结果时发生错误:", error)
-  }
+    try {
+        // console.log(JSON.stringify(results))
+        return await HLTV.getResults({ eventIds })
+    } catch (error) {
+        console.error("获取事件ID或比赛结果时发生错误:", error)
+    }
 }
 // 测试代码
 const { getEventIdsByType } = require("../getEventIdsByType.js")
 const { EventType } = require("hltv/lib/shared/EventType")
 getEventIdsByType(EventType.InternationalLAN).then((eventIds) => {
-  getResults(eventIds)
-    .then((matches) => console.log(matches))
-    .catch((error) => console.error("getResults函数中发生未处理的错误:", error))
+    getResults(eventIds)
+        .then((matches) => console.log(matches))
+        .catch((error) =>
+            console.error("getResults函数中发生未处理的错误:", error),
+        )
 })
 
 module.exports = { getResults }
